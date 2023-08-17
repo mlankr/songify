fdb0f3ebce42f306e1541526c576a2683aa99c51
Milan <milan.koirala@stud.th-luebeck.de>
2023-08-17 19:02:25 +0200

    adjust configs

--- src/public/includes/config.php
+++ src/public/includes/config.php
@@ -13,7 +13,8 @@ $timezone = date_default_timezone_set("EUROPE/BERLIN");
 
 try {
 //    $pdo = new PDO("mysql:host=$dbHost;dbname=$dbName", $dbUser, $dbPassword);
-    $pdo = new PDO('mysql:host=songify-mysql;dbname=songify', 'songify', 'songify33');
+//    $pdo = new PDO('mysql:host=songify-mysql;dbname=songify', 'songify', 'songify33');
+    $pdo = new PDO('mysql:host=milans-songify-webapp-server.mysql.database.azure.com;dbname=milans-songify-webapp-database', 'mjnltdqzww', 'RZM270OCI0A5145P$');
 } catch (PDOException $err) {
     echo "Database connection failed. " . $err->getMessage();
     exit();
